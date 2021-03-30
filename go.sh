04ce9ce1511c6ec532d7d315397c91790ee02bcd
Aaron Lipinski <aaron.lipinski@roboticsplus.co.nz>
2021-03-30 14:31:53 +1300

    episodes

--- go.sh
+++ go.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ ! -f podcast.rss ] || test "`find podcast.rss -mmin +1440`"; then
+if [ ! -f podcast.rss ] || test "`find podcast.rss -mmin +8440`"; then
     wget --no-use-server-timestamps -O podcast.rss https://ilovelearningchinese.com/feed/podcast 
 else
     echo "too new"
 fi
 
-xmllint --xpath "//channel/item/*[self::link or self::title]" podcast.rss
-
+#xmllint --xpath "//channel/item/*[self::link or self::title]" podcast.rss | xargs -L2 echo
+xmllint --xpath "//channel/item/link/text()" podcast.rss > episodes.txt
           
